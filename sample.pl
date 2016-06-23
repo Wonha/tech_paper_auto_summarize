4d83eb4728b8ccdeca8e39e6204ad93c0412b967
wonhashin <wonhashin26@gmail.com>
2016-06-23 20:29:04 +0900

    term_frequency added

---
+++ sample.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+
+
+use MeCab;
+
+my $sentence = "太郎はこの木を二郎を見た女性に渡した。";
+
+my $model = new MeCab::Model( '' );
+my $c = $model->createTagger();
+my %hash;
+
+for (my $m = $c->parseToNode($sentence); $m; $m = $m->{next}) {
+	
+	$hash{$m->{surface}}++;
+#printf("%s\t%s\n", $m->{surface}, $m->{feature});
+}
+
+for (keys %hash) {
+	print "$_ : $hash{$_}\n";
+}
